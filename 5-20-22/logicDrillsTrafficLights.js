03d86e6664aa001cb752bb0496fe89cb259963d9
Gabe <gabrielarce3@gmail.com>
2022-05-20 21:23:41 -0700

    I did a cw on a friday

---
+++ 5-20-22/logicDrillsTrafficLights.js
@@ -0,0 +1,22 @@
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// For example, update_light('green') should return 'yellow'.
+
+let updateLight = (light) => {
+    switch (light) {
+        case "green":
+            return "yellow"
+            break;
+        case "yellow":
+            return "red"
+            break;
+        case "red":
+            return "green"
+            break;
+    }
+}
+
+console.log("green leads to " + updateLight("green"))
+console.log("yellow leads to " + updateLight("yellow"))
