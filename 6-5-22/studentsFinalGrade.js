cac0a97b0bbc1cfcf0137ee910030665ade12d71
Gabe <gabrielarce3@gmail.com>
2022-06-06 22:07:10 -0700

    refactor first cw

--- 6-5-22/studentsFinalGrade.js
+++ 6-5-22/studentsFinalGrade.js
@@ -25,13 +25,12 @@
 // *Use Comparison and Logical Operators.
 
 const finalGrade = (exam, projects) => {
-    if (exam === 100 || projects > 10) {
-        return 100
+    if (exam > 90 || projects > 10) {
+        return 100;
     } else if (exam > 75 && projects >= 5) {
-        return 90
-    } else if (exam > 50 && projects > 10) {
-        return 75
-    } else {
-        return 0
+        return 90;
+    } else if (exam > 50 && projects >= 2) {
+        return 75;
     }
+    return 0;
 }
