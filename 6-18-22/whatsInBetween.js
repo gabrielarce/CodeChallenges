8d14a63f67b6f407d2f56a1f0e6babff7746e583
Gabe <gabrielarce3@gmail.com>
2022-06-18 23:05:22 -0700

    first cw today

---
+++ 6-18-22/whatsInBetween.js
@@ -0,0 +1,16 @@
+// 8 Kyu
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+const between = (a, b) => {
+    let answer = []
+    for (i = a; i <= b; i++) {
+        answer.push(i)
+    }
+    return answer
+}
