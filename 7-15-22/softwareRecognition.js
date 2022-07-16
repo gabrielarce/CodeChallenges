d89b87e8a95806adb2c34673db4d758019d27d1a
Gabe <gabrielarce3@gmail.com>
2022-07-15 21:17:51 -0700

    add cw for the day

---
+++ 7-15-22/softwareRecognition.js
@@ -0,0 +1,26 @@
+// 8 Kyu 
+
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+let correct = (string) => {
+    let arr = string.split("").map(letter => {
+        if (letter === "0") {
+            return "O"
+        } else if (letter === "5") {
+            return "S"
+        } else if (letter === "1") {
+            return "I"
+        }
+        return letter
+    })
+    return arr.join("")
+}
