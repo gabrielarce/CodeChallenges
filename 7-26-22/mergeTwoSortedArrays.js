4c4f6c54fafe1ba478d114713959d0863cf1b8bc
Gabe <gabrielarce3@gmail.com>
2022-07-27 22:42:31 -0700

    do cw for the day

---
+++ 7-26-22/mergeTwoSortedArrays.js
@@ -0,0 +1,20 @@
+// 8 Kyu 
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+// Happy coding!
+
+const mergeArrays = (arr1, arr2) => {
+    //combine both arrays
+    const arr3 = arr1.concat(arr2);
+
+    //new Set will return unique values only...then sort function sorts the array of unique values
+    return [...new Set(arr3)].sort((a, b) => a - b)
+}
