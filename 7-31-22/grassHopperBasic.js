27ba5f6948f047d2eb806de5ecf4769b013fe255
Gabe <gabrielarce3@gmail.com>
2022-08-01 19:19:49 -0700

    do one cw for the day

---
+++ 7-31-22/grassHopperBasic.js
@@ -0,0 +1,17 @@
+// 8 Kyu 
+
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+// Original Function
+// function addFive(num) {
+//     var total = num + 5
+//     return num
+//   }
+
+
+// Fixed function
+function addFive(num) {
+    return num + 5
+}
