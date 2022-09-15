d6532e73a1b47c28dc940e937d9d03cfa4674c96
Gabe <gabrielarce3@gmail.com>
2022-09-14 23:26:13 -0700

    first codewars of the day

---
+++ September/9-14-22/noOdditiesHere.js
@@ -0,0 +1,9 @@
+// 7 Kyu 
+
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+const noOdds = (arr) => {
+    return arr.filter(item => item % 2 === 0)
+}
