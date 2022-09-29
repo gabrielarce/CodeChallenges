bd300fb3caf567086727104676379b755c93dfc2
Gabe <gabrielarce3@gmail.com>
2022-09-28 21:03:12 -0700

    second codewars level 7 kyu

---
+++ September/9-28-22/anagramDetection.js
@@ -0,0 +1,20 @@
+// 7 Kyu 
+
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+// write the function isAnagram
+
+const isAnagram = function(test, original) {
+    let x = test.toLowerCase().split("").sort().join("")
+    let y = original.toLowerCase().split("").sort().join("")
+    return x.includes(y) && test.length == original.length
+};
