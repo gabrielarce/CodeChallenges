dc6b390272bdd78053caa2f92d513ea28391ea98
Gabe <gabrielarce3@gmail.com>
2022-11-07 19:52:59 -0800

    do first codewars of November

---
+++ November/11-7-22/lostNumberInSequence.js
@@ -0,0 +1,20 @@
+// Lost number in number sequence - 7 Kyu
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the starting array, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+function findDeletedNumber(arr, mixArr) {
+    if (arr.length == mixArr.length) {
+        return 0
+    }
+    let reduce1 = arr.reduce((acc, curr) => acc + curr)
+    let reduce2 = mixArr.reduce((acc, curr) => acc + curr)
+    return reduce1 - reduce2
+}
