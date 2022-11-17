ea0a6f22a86eadb40bec85afbf9223db13ee9d5d
Gabe <gabrielarce3@gmail.com>
2022-11-16 20:35:53 -0800

    do level 7 codewars

---
+++ November/11-16-22/theAverageLength.js
@@ -0,0 +1,16 @@
+// The average length - 7 Kyu
+
+// Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+// A few examples:
+
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+// If the average length is not an integer, use Math.round().
+// The input array's length > 1
+
+function averageLength(arr) {
+    let count = arr.reduce((acc, curr) => acc + curr.length, 0)
+    return arr.map(el => el[0].repeat(Math.round(count / arr.length)))
+}
