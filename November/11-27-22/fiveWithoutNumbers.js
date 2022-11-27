c87128353e526f484ba398aa2bf03c2c3b9615ac
Gabe <gabrielarce3@gmail.com>
2022-11-27 14:06:43 -0800

    do 2 codewars for Sunday

---
+++ November/11-27-22/fiveWithoutNumbers.js
@@ -0,0 +1,9 @@
+// 8 Kyu - 5 Without Numbers 
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+function unusualFive() {
+    return "hello".length
+}
