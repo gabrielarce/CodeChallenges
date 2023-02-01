89eebf1d282ceb33a291a7bc8abd370940f38324
Gabe <gabrielarce3@gmail.com>
2023-01-31 23:58:23 -0800

    daily code challenge

---
+++ January/1-31-23/greatestCommonDivisor.js
@@ -0,0 +1,14 @@
+// 7 Kyu - Greatest Common Divisor 
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+
+function gcd(a, b) {
+    while (b !== 0) {
+        const remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+    return a;
+}
