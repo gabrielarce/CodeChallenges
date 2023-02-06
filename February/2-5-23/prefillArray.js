6144c6fe88175f7c5efab14182e4a9ca384f3094
Gabe <gabrielarce3@gmail.com>
2023-02-06 00:02:01 -0800

    daily code challenge

--- February/2-5-23/prefillArray.js
+++ February/2-5-23/prefillArray.js
@@ -1,3 +1,5 @@
+// 6 Kyu - Prefill An Array
+
 // Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
 
 // You have to validate input:
