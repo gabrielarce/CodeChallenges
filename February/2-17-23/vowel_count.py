f8fad057feef16b2bb5f99e00ef343545e3e96ef
Gabe <gabrielarce3@gmail.com>
2023-02-17 18:52:35 -0800

    daily code challenge - python

---
+++ February/2-17-23/vowel_count.py
@@ -0,0 +1,13 @@
+# Return the number (count) of vowels in the given string.
+
+# We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+# The input string will only consist of lower case letters and/or spaces.
+
+def get_count(sentence):
+    vowels = "aeiou"
+    counter = 0
+    for letter in sentence:
+        if letter in vowels:
+            counter += 1
+    return counter
