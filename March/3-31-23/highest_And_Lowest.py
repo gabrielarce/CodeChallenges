939c3aa152f3ba74bd6b1a20fd1648bff0d9222b
Gabe <gabrielarce3@gmail.com>
2023-03-31 19:50:05 -0700

    daily code challenge

---
+++ March/3-31-23/highest_And_Lowest.py
@@ -0,0 +1,28 @@
+# 7 kyu - Highest and Lowest  
+# Description:
+
+# In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+# Examples
+
+# high_and_low("1 2 3 4 5")  # return "5 1"
+# high_and_low("1 2 -3 4 5") # return "5 -3"
+# high_and_low("1 9 3 4 -5") # return "9 -5"
+
+# Notes
+
+#     All numbers are valid Int32, no need to validate them.
+#     There will always be at least one number in the input string.
+#     Output string must be two numbers separated by a single space, and highest number is first.
+
+def high_and_low(numbers):
+    nums = numbers.split(" ")
+    high = int(nums[0])
+    low = int(nums[0])
+    for num in nums: 
+        num_int = int(num)
+        if num_int > high: 
+            high = num_int
+        if num_int < low:
+            low = num_int
+    return str(high) + " " + str(low)
+
