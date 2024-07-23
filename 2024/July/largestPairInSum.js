f4487d5d1d816dd55524e427c3e28cec1f1986c1
Gabe <gabrielarce3@gmail.com>
2024-07-22 23:35:04 -0700

    daily code challenge, in javascript :)

--- 2024/July/largestPairInSum.js
+++ 2024/July/largestPairInSum.js
@@ -9,6 +9,9 @@
 // Input sequence contains minimum two elements and every element is an integer.
 
 function largestPairSum(numbers) {
+  // Create an variable that holds a sorted copy (descending) of the numbers array
   let descending = numbers.sort((a, b) => b - a);
+
+  // Return the sum of the first 2 elements of the array
   return descending[0] + descending[1];
 }
