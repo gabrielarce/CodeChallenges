3d41f64eb90a5ec7236467140fe47b817063c91a
Gabe <gabrielarce3@gmail.com>
2024-07-22 23:09:57 -0700

    daily code challenge, in javascript :)

---
+++ 2024/July/largestPairInSum.js
@@ -0,0 +1,14 @@
+// 6 Kyu - Largest Pair in Sum
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+// [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+
+// Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum(numbers) {
+  let descending = numbers.sort((a, b) => b - a);
+  return descending[0] + descending[1];
+}
